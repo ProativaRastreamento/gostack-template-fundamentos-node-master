96c988f20518f0ed5e14e9c84a24a88e4070601c
proativarastreamento <adm@proativarastreamento.com>
2020-06-28 19:07:34 -0300

    gostack-fundamentos-node-master

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,35 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Tipo de transação invalida');
+    }
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw new Error('Voce não tem sauldo sufuciente!');
+    }
+
+    const tranaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    return tranaction;
+  }
+}
+
+export default CreateTransactionService;
